6252189472bff8798f37fd444ff7dc2216b6d976
saarthak10 <saarthaksharma87@gmail.com>
2026-03-11 21:43:49 +0530

    console added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import { AGENTS, HOME_COPY, PRICING_BASKETS } from '../data/content'
 import { motion } from 'framer-motion'
 
 export default function Home() {
+  console.log('Home page rendered')
   return (
     <main>
       <section className="relative">
